b4adaf47c8830bd98cb753569e5d8fd5f098eebf
Uncle Scientist <unclescientist@gmail.com>
2022-10-13 08:18:06 -0400

    alternate solution for Ransom Note

--- src/lc_0383_ransom_note.rs
+++ src/lc_0383_ransom_note.rs
@@ -2,10 +2,33 @@ pub struct Solution;
 
 impl Solution {
     pub fn can_construct(ransom_note: String, magazine: String) -> bool {
+        use std::collections::hash_map::Entry;
         use std::collections::HashMap;
 
         let mut hm = HashMap::new();
 
+        let mut count = 0;
+        for ch in ransom_note.chars() {
+            *hm.entry(ch).or_insert(0) += 1;
+            count += 1;
+        }
+
+        for ch in magazine.chars() {
+            if let Entry::Occupied(mut e) = hm.entry(ch) {
+                if *e.get() > 0 {
+                    *e.get_mut() -= 1;
+                    count -= 1;
+                    if count == 0 {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        false
+
+        /*
+         * Alternate solution:
         for ch in magazine.chars() {
             *hm.entry(ch).or_insert(0) += 1;
         }
@@ -19,6 +42,7 @@ impl Solution {
         }
 
         true
+        */
     }
 }
 
