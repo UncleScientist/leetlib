c8ed4becc8336b0fafcade1d8cb1778b81bdf902
Uncle Scientist <unclescientist@gmail.com>
2022-10-19 08:26:48 -0400

    a slightly more efficient approach

--- src/lc_0542_01_matrix.rs
+++ src/lc_0542_01_matrix.rs
@@ -2,10 +2,14 @@ pub struct Solution;
 
 impl Solution {
     pub fn update_matrix(mat: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        use std::collections::HashSet;
+
         let width = mat[0].len();
         let height = mat.len();
         let mut output = vec![vec![0; width]; height];
         let max = (width + height) as i32;
+
+        let mut hs = HashSet::new();
 
         for row in 0..height {
             for col in 0..width {
@@ -13,32 +17,38 @@ impl Solution {
                     output[row][col] = 0;
                 } else {
                     output[row][col] = max;
+                    hs.insert((row, col));
                 }
             }
         }
 
-        let mut changed = true;
-        while changed {
-            changed = false;
-            for row in 0..height {
-                for col in 0..width {
-                    let up = if row == 0 { max } else { output[row - 1][col] };
-                    let down = if row + 1 == height {
-                        max
-                    } else {
-                        output[row + 1][col]
-                    };
-                    let left = if col == 0 { max } else { output[row][col - 1] };
-                    let right = if col + 1 == width {
-                        max
-                    } else {
-                        output[row][col + 1]
-                    };
+        while !hs.is_empty() {
+            let entries = hs.drain().collect::<Vec<(usize, usize)>>();
 
-                    let min = 1 + up.min(down.min(left.min(right)));
-                    if output[row][col] > min {
-                        output[row][col] = min;
-                        changed = true;
+            for (row, col) in entries {
+                let up = if row == 0 { None } else { Some((row - 1, col)) };
+                let down = if row + 1 == height {
+                    None
+                } else {
+                    Some((row + 1, col))
+                };
+                let left = if col == 0 { None } else { Some((row, col - 1)) };
+                let right = if col + 1 == width {
+                    None
+                } else {
+                    Some((row, col + 1))
+                };
+
+                let mut min = max;
+                for dir in [up, down, left, right].iter().filter_map(|&x| x) {
+                    min = min.min(output[dir.0][dir.1]);
+                }
+                min += 1;
+
+                if min < output[row][col] {
+                    output[row][col] = min;
+                    for dir in [up, down, left, right].iter().filter_map(|&x| x) {
+                        hs.insert(dir);
                     }
                 }
             }
